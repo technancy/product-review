3c8f3e5eaf732befa1325f6b1ea42c027f382c5d
technancy <nancytechy@gmail.com>
2017-05-19 10:47:12 +0100

    Add a Procfile

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: ./gradle install run
---
+++ src/test/java/technancy/javatraining/resources/ProductResourceTest.java
@@ -0,0 +1,12 @@
+package technancy.javatraining.resources;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ProductResourceTest {
+    @Test
+    public void getAProduct() throws Exception {
+
+    }
+}
